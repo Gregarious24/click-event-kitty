4b1c644c530f49e4e040e16dfbfffacc5225263d
Greg M <142060854+Gregarious24@users.noreply.github.com>
2023-10-05 15:38:34 -0700

    Added if & else statements for button and cat image

--- js/script.js
+++ js/script.js
@@ -5,7 +5,16 @@ var cat = document.querySelector(".cat");
 // console.log(cat);
 
 button.addEventListener("click", function() {
-    // console.log("It's working.");
+   if (cat.classList.contains("show")) {
+    // console.log("Yes.")
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+   } else {
+    // console.log("No.")
     cat.classList.add("show");
+    button.innerText = "Shoo, cat!!";
+    button.classList.remove("disappear");
+   }
 })
 
